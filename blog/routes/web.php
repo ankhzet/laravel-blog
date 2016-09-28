00cb3d04989861dcf396d4a712a38775e8587b40
ankhzet <ankhzet@gmail.com>
2016-09-28 22:00:25 +0300

    Fix: remove redundant 'web' group in routes

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-Route::group(['middleware' => ['web']], function () {
 	Route::any('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 
 	// users
@@ -8,7 +7,6 @@ Route::group(['middleware' => ['web']], function () {
 	Route::post('/login', ['as' => 'users.login', 'uses' => 'Auth\LoginController@login']);
 	Route::get('/logout', ['as' => 'users.logout', 'uses' => 'Auth\LoginController@logout']);
 
-});
 
 	Route::get('admin/login', ['as' => 'users.admin-login-form', 'uses' => 'Auth\LoginController@showAdminLoginForm']);
 	Route::post('admin/login', ['as' => 'users.admin-login-form', 'uses' => 'Auth\LoginController@login']);
