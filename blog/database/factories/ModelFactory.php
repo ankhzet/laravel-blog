22e3789f7af2d64fd35de7e34c56e8dc27f2ca3b
ankhzet <ankhzet@gmail.com>
2016-09-28 22:02:39 +0300

    Fix: seed db with 'secret' as user password

--- blog/database/factories/ModelFactory.php
+++ blog/database/factories/ModelFactory.php
@@ -4,7 +4,7 @@ $factory->define(Blog\User::class, function (Faker\Generator $faker) {
 	return [
 		'name' => $faker->name,
 		'email' => $faker->unique()->safeEmail,
-		'password' => bcrypt($faker->password),
+		'password' => bcrypt('secret'),
 		'remember_token' => str_random(10),
 	];
 });
