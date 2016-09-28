0c158f7e6c75dd5001c5eaffd24520eb6767449b
ankhzet <ankhzet@gmail.com>
2016-09-28 22:35:03 +0300

    Add Post migration, model and controller

---
+++ blog/app/Http/Controllers/PostsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Blog\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Blog\Post;
+
+class PostsController extends Controller {
+
+}
---
+++ blog/database/seeds/PostsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+
+		$user = Blog\User::first();
+
+		factory(Blog\Post::class, 25)->create(['user_id' => $user->id])->each(function($post) use ($user) {
+		});
+
+	}
+
+}
